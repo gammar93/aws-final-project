0806ebb5aa40d9178f17d6869910a45fe03ce4e2
gammar93 <gammargaryagdyyev@gmail.com>
2024-08-15 07:49:22 +0300

    endpoint added

--- BlogDjango/src/cblog/settings.py
+++ BlogDjango/src/cblog/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'fprds', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here 
         'PASSWORD': config('PASSWORD'), # FinalProject2024!
-        'HOST': 'xxxxxxxxxx',  # database endpoint is written here
+        'HOST': 'fprds.cdmc8scig7p8.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
