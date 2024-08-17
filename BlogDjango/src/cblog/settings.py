ed88c570c6203540b0a5c3ddced06585641e5d59
gammar93 <gammargaryagdyyev@gmail.com>
2024-08-17 03:36:21 +0300

    new endpoint

--- BlogDjango/src/cblog/settings.py
+++ BlogDjango/src/cblog/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fpdb', # database name in RDS is written here
+        'NAME': 'fprds', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here 
         'PASSWORD': config('PASSWORD'), # FinalProject2024!
-        'HOST': 'fpdb.cdmc8scig7p8.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'HOST': 'fprds.cdmc8scig7p8.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
